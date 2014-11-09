ed9a20baa1d3e24e08dbede58a628fea56e8d9f8
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-11-09 12:56:36 +0300

    Encoding benchmark

---
+++ executables/Encoding.hs
@@ -0,0 +1,38 @@
+module Main where
+
+import BasePrelude
+import MTLPrelude
+import Control.DeepSeq
+import Criterion
+import Criterion.Main
+import Data.Text (Text)
+import Data.ByteString (ByteString)
+import Data.Scientific (Scientific)
+import Data.Time
+import qualified PostgreSQLBinary.Encoder as E
+import qualified PostgreSQLBinary.Decoder as D
+
+
+main =
+  defaultMain
+    [
+      b "bool"        E.bool        True,
+      b "int2"        E.int2        (Left 1000),
+      b "int4"        E.int4        (Left 1000),
+      b "int8"        E.int8        (Left 1000),
+      b "float4"      E.float4      12.65468468,
+      b "float8"      E.float8      12.65468468,
+      b "numeric"     E.numeric     (read "20.213290183"),
+      b "char"        E.char        'Я',
+      b "text"        E.text        (Left "alsdjflskjдывлоаы оады"),
+      b "bytea"       E.bytea       (Left "alskdfj;dasjfl;dasjflksdj"),
+      b "date"        E.date        (read "2000-01-19"),
+      b "time"        E.time        (read "10:41:06"),
+      b "timetz"      E.timetz      (read "(10:41:06, +0300)"),
+      b "timestamp"   E.timestamp   (read "2000-01-19 10:41:06"),
+      b "timestamptz" E.timestamptz (read "2000-01-19 10:41:06"),
+      b "interval"    E.interval    (secondsToDiffTime 23472391470128374)
+    ]
+  where
+    b name encoder value = 
+      bench name $ nf encoder value
